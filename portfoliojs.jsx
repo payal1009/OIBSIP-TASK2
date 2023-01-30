6c9bb18178bfb51d11b447d8ce645a9726623da2
payal1009 <122307737+payal1009@users.noreply.github.com>
2023-01-30 22:07:48 +0530

    Add files via upload

---
+++ portfoliojs.jsx
@@ -0,0 +1,40 @@
+function myFunction1() {
+    document.getElementById('f_table').style.display = "block";
+    var x = document.getElementById("list_1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myFunction2() {
+    document.getElementById('f_table2').style.display = "block";
+    var x = document.getElementById("list_2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myFunction3() {
+    document.getElementById('f_table3').style.display = "block";
+    var x = document.getElementById("list_3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myFunction4() {
+    document.getElementById('f_table4').style.display = "block";
+    var x = document.getElementById("list_4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
